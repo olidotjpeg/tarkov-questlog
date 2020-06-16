b6d6a17cb4babc7c345ad30ab7848ce8f68e4ee5
Oliver Munkebo <o.munkebo@eslgaming.com>
2020-06-16 15:31:18 +0200

    Added quest completed section

--- src/QuestLog.js
+++ src/QuestLog.js
@@ -9,14 +9,24 @@ export default ({ quests, traderName, updateQuest }) => {
   return (
     <div>
       {quests.length > 0 ? quests.map((quest) => {
-        return (
-          <Box key={quest.name} onClick={() => updateQuest(quest, traderName)}>
-            <p>{quest.name}</p>
-            {quest.objectives.map((q) => <p key={q}>{q}</p>)}
-            {quest.state ? <p>True</p> : <p>False</p>}
-          </Box>
-        )
+        return !quest.state ? (
+            <Box key={quest.name} onClick={() => updateQuest(quest, traderName)}>
+              <p>{quest.name}</p>
+              {quest.objectives.map((q) => <p key={q}>{q}</p>)}
+              {quest.state ? <p>True</p> : <p>False</p>}
+            </Box>
+        ) : <React.Fragment />
       }) : <p>We have no quests yet for this trader</p>}
+      <p>Completed Quests</p>
+      {quests.length > 0 ? quests.map((quest) => {
+        return quest.state ? (
+            <Box key={quest.name} onClick={() => updateQuest(quest, traderName)}>
+              <p>{quest.name}</p>
+              {quest.objectives.map((q) => <p key={q}>{q}</p>)}
+              {quest.state ? <p>True</p> : <p>False</p>}
+            </Box>
+        ) : <React.Fragment />
+      }) : <React.Fragment />}
     </div>
   )
 };
