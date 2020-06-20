45c9e15fb928cb0a5de9d23f919ced16a9ecbfc3
Oliver Munkebo <o.munkebo@eslgaming.com>
2020-06-20 15:41:30 +0200

    build added

---
+++ build/precache-manifest.578c4d2b8ce737c0eca437652ab28064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa5779bfae5d14e7fd1db912a6d2c14",
+    "url": "/tarkov-questlog/index.html"
+  },
+  {
+    "revision": "e527965008508158aa36",
+    "url": "/tarkov-questlog/static/css/main.ba903ef8.chunk.css"
+  },
+  {
+    "revision": "a25d39f5f3eda83a09c4",
+    "url": "/tarkov-questlog/static/js/2.c1973d04.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tarkov-questlog/static/js/2.c1973d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e527965008508158aa36",
+    "url": "/tarkov-questlog/static/js/main.c6ab031d.chunk.js"
+  },
+  {
+    "revision": "b2a40439c80742274b0b",
+    "url": "/tarkov-questlog/static/js/runtime-main.8b205891.js"
+  }
+]);
