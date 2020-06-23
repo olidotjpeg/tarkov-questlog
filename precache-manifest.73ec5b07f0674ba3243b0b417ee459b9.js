373d113058a4ca91f7667a0d62e49578a0671fd6
Oliver Munkebo <o.munkebo@eslgaming.com>
2020-06-23 09:31:31 +0200

    Updates

--- precache-manifest.73ec5b07f0674ba3243b0b417ee459b9.js
+++ precache-manifest.73ec5b07f0674ba3243b0b417ee459b9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc343fca3e95bcb0d07868cc6135c104",
+    "revision": "7c3556f4e7447af8316914ca742bdf7a",
     "url": "/tarkov-questlog/index.html"
   },
   {
-    "revision": "b6b200f63f4c33ce0285",
+    "revision": "31b133a608dd8831b3a4",
     "url": "/tarkov-questlog/static/css/main.ba903ef8.chunk.css"
   },
   {
-    "revision": "a25d39f5f3eda83a09c4",
-    "url": "/tarkov-questlog/static/js/2.c1973d04.chunk.js"
+    "revision": "5c404a3d49b5998a9a5c",
+    "url": "/tarkov-questlog/static/js/2.1249fc91.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/tarkov-questlog/static/js/2.c1973d04.chunk.js.LICENSE.txt"
+    "url": "/tarkov-questlog/static/js/2.1249fc91.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b6b200f63f4c33ce0285",
-    "url": "/tarkov-questlog/static/js/main.c959f5ba.chunk.js"
+    "revision": "31b133a608dd8831b3a4",
+    "url": "/tarkov-questlog/static/js/main.3e906262.chunk.js"
   },
   {
     "revision": "b2a40439c80742274b0b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tarkov-questlog/precache-manifest.2d84ce744992d6ccdb50fc4381b89c1f.js"
+  "/tarkov-questlog/precache-manifest.73ec5b07f0674ba3243b0b417ee459b9.js"
 );
 
 self.addEventListener('message', (event) => {
